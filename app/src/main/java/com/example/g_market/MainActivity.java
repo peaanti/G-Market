541f92bed14896500c38a94c7e0da609fbf6089c
peacedeadzki <matmir04@yandex.ru>
2021-01-09 13:22:12 +0300

    Left navigation bar, top bar

---
+++ app/src/main/java/com/example/g_market/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.g_market;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        
+        findViewById(R.id.imageMenu).setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+    }
+}
